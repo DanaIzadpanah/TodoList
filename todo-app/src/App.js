94d4c8c84ff449d3d191f1c8ac436061795adcec
Dana Izadpanah <danaizad8624@gmail.com>
2023-12-26 13:58:15 -0600

    Making multiple todos available

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -22,6 +22,24 @@ function App() {
     }
   ]);
 
+  function handleKeyDown(e, i) {
+    if(e.key === 'Enter'){
+      createTodoAtIndex(e,i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
   return (
     <div className="app">
       <header className="header">
@@ -29,12 +47,16 @@ function App() {
       </header>
       <form className='todo-list'>
         <ul>
-          
+
           {todos.map((todo, i) => (
 
           <div className='todo'>
             <div className='checkbox' />
-            <input type='text' value={todo.content}/>
+            <input
+              type="text"
+              value={todo.content}
+              onKeyDown={e => handleKeyDown(e, i)}
+            />
           </div>
           ))}
 
